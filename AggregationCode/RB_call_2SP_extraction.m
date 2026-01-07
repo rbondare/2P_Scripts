f4b216e10ad9088f610937ddc4f2609094fbeaae
Rimma Bondarenko <rimmabondarenkoo@gmail.com>
2026-01-07 22:20:14 +0100

    comments added

--- AggregationCode/RB_call_2SP_extraction.m
+++ AggregationCode/RB_call_2SP_extraction.m
@@ -1,15 +1,27 @@
+
+%% ==== This sript aggregates behaviour, stimulus and calcium data together into 1 preprocessed file === 
+% dependencies: 
+% RB_import_VideoBehav.m
+% RB_save2Pout_and_headers.m
+
+% required inputs- STIMULUS (stores stimulus files), DLC folder (contains
+% DLC extraction results from the DLC computer)
+% TIF + HEADERS (headers extracted from TIF files on the 2P computer where scanimage runs). The animal list for file aggregation is pulled from here.
+% ANIMALS folder (stores animal data entered into FS_PotentScript. This folder is maintained locally on the DLC computer and must be transferred to the group drive).
+
 % Setup paths
 addpath('C:\Users\rbondare\2P_scripts\');
 addpath('Z:\Group Members\Rima\Stimulus'); 
 addpath('C:\Users\rbondare\2P_scripts\MouseBrainActivity-main\2p Pre-processing');
 
+AnimalFolder = 'Z:\Group Members\Rima\Animals\';
 maindir = 'Z:\Group Members\Rima\TEST\'; %DATA_2P
 ca_type = 1;
 
 % Get all experiment folders
 explist = dir(maindir);
 explist = explist(cat(1,explist.isdir));
-explist = explist(~ismember({explist.name},{'. ','..'}));  % FIXED directory filtering
+explist = explist(~ismember({explist.name},{'. ','..'}));  
 
 if isempty(explist)
     error('No experiments found in %s', maindir);
@@ -17,12 +29,11 @@ end
 
 fprintf('Found %d recording(s) to process\n', numel(explist));
 
-AnimalFolder = 'Z:\Group Members\Rima\Animals\';
 parallel. gpu.enableCUDAForwardCompatibility(true);
 params = FS_load_default_settings();
 
-params. Neurons.use_deconvolved = true;
-params.Neurons. activity_type = 'both';
+params.Neurons.use_deconvolved = true;
+params.Neurons.activity_type = 'both';
 
 %% Process ALL recordings
 for e = 1:numel(explist)  
